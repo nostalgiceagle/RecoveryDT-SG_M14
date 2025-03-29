b5b89b7402adcdc7e7166b7511cd26d92504d1be
nostalgiceagle <localhost@pritam.com>
2025-03-29 11:15:46 +0530

    fixup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/twrp_a10.mk
+	$(LOCAL_DIR)/twrp_a05s.mk
 
 COMMON_LUNCH_CHOICES := \
-     twrp_a10-user \
-     twrp_a10-userdebug \
-     twrp_a10-eng
+     twrp_a05s-user \
+     twrp_a05s-userdebug \
+     twrp_a05s-eng
